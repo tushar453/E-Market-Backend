e7a7a46092da9ecd988002c9b6b8cbec6c22da7c
SharmaTushar <sharmahimesh282@gmail.com>
2023-12-30 11:36:11 +0530

    added

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 //  mongoose.connect("mongodb://127.0.0.1:27017/Hotel-Room")
 //   mongoose.connect("mongodb+srv://sharmatushar3454:Tushar2002@hotel-book.bbxhqnn.mongodb.net/Hotel-Room?retryWrites=true&w=majority")
 // mongoose.connect("mongodb://127.0.0.1:27017/MernChat")
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect("mongodb+srv://sharmatushar3454:Tushar2002@hotel-book.bbxhqnn.mongodb.net/Ecommerce?retryWrites=true&w=majority")
 
 var connection = mongoose.connection;
 
